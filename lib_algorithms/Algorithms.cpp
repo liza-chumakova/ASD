15ad51758eeffd2e55ac60d22569938289a293aa
Liza Chumakova <lisa.chumakova@gmail.com>
2025-10-28 20:55:36 +0300

    add algorithms implementation

--- lib_algorithms/Algorithms.cpp
+++ lib_algorithms/Algorithms.cpp
@@ -9,7 +9,7 @@ int main()
 }
 
 bool check_same(std::string left, std::string right) {
-    if (((left == "(") && (right == ")")) || ((left == "{") && (right == "}")) || ((left == "[") && (right == "]"))) {
+    if (((left == '(') && (right == ')')) || ((left == '{') && (right == '}')) || ((left == '[') && (right == ']'))) {
         return true;
     }
     return false;
@@ -19,28 +19,42 @@ bool check_breckets(std::string toCheck) {
     Stack<std::string> checking(toCheck.size());
     if (toCheck.size() == 0) {return true;}
     //Проверка если закрывающие в начале или открывающие в конце
-    //if (check_same(toCheck[0], ")") || check_same(toCheck[0], "}") || check_same(toCheck[0], "]")) {return false}
-    //if (check_same(toCheck[toCheck.size() - 1], "(") || check_same(toCheck[toCheck.size() - 1], "{") || check_same(toCheck[toCheck.size() - 1], "[")) {return false}
+    if (check_same(toCheck[0], ')') || check_same(toCheck[0], '}') || check_same(toCheck[0], ']')) {return false}
+    if (check_same(toCheck[toCheck.size() - 1], '(') || check_same(toCheck[toCheck.size() - 1], '{') || check_same(toCheck[toCheck.size() - 1], '[')) {return false}
     //В ином случае
     if (toCheck.size()%2 == 1) {return false};
     else{
         for (int i = 0; i < toCheck.size(); ++i) {
-            if (check_same(toCheck[i], "(") || check_same(toCheck[i], "{") || check_same(toCheck[i], "[")) {
+            if ((toCheck[i]== '(') || (toCheck[i]== '{') || (toCheck[i]== '[')) {
                 checking.push(toCheck[i]);
-                for (int j = i; j < toCheck.size(); ++j) {
-                    if (check_same(checking.top(), toCheck[j])) {
-                        checking.pop();
-                        toCheck.erase(j,1);
-                        break;
-                    }
+            }
+            //     for (int j = i; j < toCheck.size(); ++j) {
+            //         if (check_same(checking.top(), toCheck[j])) {
+            //             checking.pop();
+            //             toCheck.erase(j,1);
+            //             break;
+            //         }
+            //     }
+            // }
+            //else if ((toCheck[i] == '(') || (toCheck[i] == '{') || (toCheck[i] == '[')) {
+            else {
+                if (checking.is_empty()) {return false}
+
+                char ch = checking.top();
+                checking.pop();
+
+                if ((toCheck[i] == ')' && ch != '(') || (toCheck[i] == '}' && ch != '{') || (toCheck[i] == ']' && ch != '[')) {
+                    return false;
                 }
+
             }
         }
-        if (checking.is_empty() && (toCheck.find(")") == 0) && (toCheck.find("}") == 0) && (toCheck.find("}") == 0)) {
-            return true;
+        // if (checking.is_empty() && (toCheck.find(')') == 0) && (toCheck.find('}') == 0) && (toCheck.find('}') == 0)) {
+        //     return true;
+        // }
+        // else {
+        //     return false;
+        return checking.is_empty();
         }
-        else {
-            return false;
-        }
-    }
+    
 }
